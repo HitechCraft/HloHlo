78f9f2ae66521e7b26021d1709860a9e1700ea45
nibytes <nickita.davidenko@gmail.com>
2015-10-20 14:40:13 +0100

    update lot model

--- hlohlo_main/models.py
+++ hlohlo_main/models.py
@@ -3,17 +3,40 @@ from extuser.models import ExtUser
 
 
 class Lot(models.Model):
-    name = models.CharField('Название', max_length=255)
-    description = models.TextField('Описание')
-    type_auction = models.BooleanField('Купить сейчас', default=False)
-    time_create = models.DateTimeField('Дата создания')
-    time_life = models.IntegerField('Прошло времени')
-    price = models.FloatField('Цена')
+    name = models.CharField(
+        'Название',
+        max_length=255
+    )
+    description = models.TextField(
+        'Описание'
+    )
+
+    type_auction = models.BooleanField(
+        'Купить сейчас',
+        default=False
+    )
+    time_create = models.DateTimeField(
+        'Дата создания'
+    )
+    time_life = models.IntegerField(
+        'Прошло времени'
+    )
+    price = models.FloatField(
+        'Цена'
+    )
     photos = []
     category = []
-    count_viewers = models.IntegerField('Посетители')
-    author = models.OneToOneField(ExtUser, related_name='author_profile')
-    buyer = models.OneToOneField(ExtUser, related_name='buyer_profile')
+    count_viewers = models.IntegerField(
+        'Посетители'
+    )
+    author = models.OneToOneField(
+        ExtUser,
+        related_name='author_profile'
+    )
+    buyer = models.OneToOneField(
+        ExtUser,
+        related_name='buyer_profile'
+    )
     # Subscribers = ForeignKey(ExtUser, related_name='subscriber_profiles')
     def __str__(self):
         return self.name
